ba384b53f45c321d0dcd539fc44693dfa6a5ed1d
Archer <Crossroads13666@gmail.com>
2023-08-19 23:54:32 +0300

    Added sources

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "check_move.h"
+#include "ant_path.h"
+
+
+
+int main(){
+
+    std::cout << "Starting move............." << std::endl;
+    Coordinates stay_now(1000, 1000);
+    AntPath ant(stay_now);
+    ant.ExploreWorld();
+
+    std::getchar();
+return 0;
+}
